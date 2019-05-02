0917974005183eba89f14e3768af2f0d61745df7
devindraizin <devindraizin@knights.ucf.edu>
2019-05-02 10:17:31 -0400

    Moved .ino

---
+++ Sketchbook/motorTest/motorTest.ino
@@ -0,0 +1,26 @@
+#include "revisedMotorHeader.h"
+
+double input,output,setpoint = 3.0,input2,output2,setpoint2 = 3.0;
+double Kp = 2.5,Ki = 62.5,Kd = 0.0;
+Adafruit_MotorShield *AFMS = new Adafruit_MotorShield();
+
+rMotor Motor1 = rMotor(2,23,3072,&input,&output,&setpoint,Kp,Ki,Kd,DIRECT);
+
+
+void setup()
+{
+    AFMS->begin();
+
+    Motor1.setAfms(AFMS);
+
+    Motor1.registerMotor();
+
+    Serial.begin(9600);
+    Serial.println("A lot of words");
+
+}
+
+void loop()
+{
+    Motor1.setDuty(150);
+}
