3351ec42ad22044c73df75b2ee80ee6be6db8889
ryanketzner <rketzner@knights.ucf.edu>
2019-05-02 16:57:21 -0400

    Matlab connection is working, motors are turning, encoder feedback is working and correct.

--- ArduinoInterface/ArduinoSetup.m
+++ ArduinoInterface/ArduinoSetup.m
@@ -1,8 +1,8 @@
 
 a = arduino('com3','Mega2560','Libraries','MatlabMotorLibrary/EncoderAddon','ForceBuild',true,'TraceOn',true)
 Motor1 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D2','D23'})
-Motor2 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D18','D25'})
-Motor3 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D19','D27'})
+Motor2 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D19','D27'})
+Motor3 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D18','D25'})
 
 Motor1.getCounts()
 Motor2.getCounts()
