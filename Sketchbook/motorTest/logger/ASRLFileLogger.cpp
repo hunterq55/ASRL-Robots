e875645a881204a18d9d2222b08b0299d88124e3
devindraizin <devindraizin@knights.ucf.edu>
2019-05-14 20:44:18 -0400

    Real logger for realsies this time

--- Sketchbook/motorTest/logger/ASRLFileLogger.cpp
+++ Sketchbook/motorTest/logger/ASRLFileLogger.cpp
@@ -21,9 +21,18 @@ void logger::closeFile()
 
 void logger::logMessage(std::string message)
 {
-    std::time_t result = std::time(nullptr);;
-    file << result;
-    file << "\n";
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[80];
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);
+    std::string str(buffer);
+
+    file << str;
+    file << " -> ";
     file << message;
     file << "\n";
 }
