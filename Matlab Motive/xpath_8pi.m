21827efd387e2333004a5c81c52e08b451b660a5
hunterq55 <40833934+hunterq55@users.noreply.github.com>
2019-05-22 09:18:25 -0400

    Changed calculations for path estimate

--- Matlab Motive/xpath_8pi.m
+++ Matlab Motive/xpath_8pi.m
@@ -1,2 +1,2 @@
 function out = xpath_8pi(t,x_start)
-out= cos(0.25*t+((3*pi)/2))+x_start;
+out= sin(0.25*t)+x_start;
--- Matlab Motive/ypath_8pi.m
+++ Matlab Motive/ypath_8pi.m
@@ -1,2 +1,2 @@
 function out = ypath_8pi(t,y_start)
-out = sin(0.25*t+(pi)/2)+y_start-1;
+out = cos(0.25*t)+y_start+1;
