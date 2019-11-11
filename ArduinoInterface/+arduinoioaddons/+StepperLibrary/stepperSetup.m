27b5bf36420aa9a5aad2d46c85db3fbb63b6e0ca
hunterq55 <quebedeauxh55@gmail.com>
2019-11-11 13:43:35 -0500

    Added Command in Comments so you don't forget

--- ArduinoInterface/+arduinoioaddons/+StepperLibrary/stepperSetup.m
+++ ArduinoInterface/+arduinoioaddons/+StepperLibrary/stepperSetup.m
@@ -11,6 +11,7 @@ Stepper5 = addon(a,'StepperLibrary/Stepper',{'D10','D11'})
 Stepper6 = addon(a,'StepperLibrary/Stepper',{'D12','D13'})
 
 Stepper1.calibrate();
+% Stepper1.set()
 %[0,-110*pi/180,141*pi/180,0,0,0] rest position
 STEPPER_CONSTANT1=[2561.45838425888];
 STEPPER_CONSTANT2=[3168.63019224010];
