a155caecf2562eafce68bf667ae04690a2972a30
hunterq55 <hunterq@knights.ucf.edu>
2019-11-27 01:24:05 -0500

    Updated Library

--- ArduinoInterface/ArduinoSetup.m
+++ ArduinoInterface/ArduinoSetup.m
@@ -1,4 +1,4 @@
-a = arduino('com6','Mega2560','Libraries','MatlabMotorLibrary/EncoderAddon','ForceBuild',true,'TraceOn',true);
+a = arduino('com8','Mega2560','Libraries','MatlabMotorLibrary/EncoderAddon','ForceBuild',true,'TraceOn',true);
 Motor1 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D2','D23'});
 Motor2 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D19','D27'});
 Motor3 = addon(a,'MatlabMotorLibrary/EncoderAddon',{'D18','D25'});
