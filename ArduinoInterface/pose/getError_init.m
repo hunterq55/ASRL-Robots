690d0349962c45337c7c42295a3fb227f3847f78
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2020-10-22 14:07:09 -0400

    Updated to support new FK and Jacobian functions

--- ArduinoInterface/pose/getError_init.m
+++ ArduinoInterface/pose/getError_init.m
@@ -1,7 +1,7 @@
 % Function to calculate the inital error in the effector pose
 
-function err_init = getError_init(x_ref, theta_ref, q_init)
-[x_init, theta_init] = AR2fkine(q_init);
+function err_init = getError_init(x_ref, theta_ref, q_init,Robot)
+[x_init, theta_init] = AR2fkine(q_init,Robot);
 ex_init = x_ref - x_init;
 C_ref = eul2r(theta_ref');
 C_init = eul2r(theta_init');
--- ArduinoInterface/pose/poseTrackingOpen.m
+++ ArduinoInterface/pose/poseTrackingOpen.m
@@ -7,7 +7,22 @@ function qSaved = poseTrackingOpen(path_AR2_J,Stepper1)
 %Stepper1 is the stepper motor object that defines the stepper motors in
 %matlab using the custom library
 
+%% Robot Definition
+d1 = 169.77;    a1 = 64.2;  alpha1 = -90*pi/180;    
+d2 = 0;         a2 = 305;   alpha2 = 0;             
+d3 = 0;         a3 = 0;     alpha3 = 90*pi/180;     
+d4 = -222.63;   a4 = 0;     alpha4 = -90*pi/180;    
+d5 = 0;         a5 = 0;     alpha5 = 90*pi/180;     
+d6 = -36.25;    a6 = 0;     alpha6 = 0;  
 
+L(1) = Link([0 d1 a1 alpha1], 'R');
+L(2) = Link([0 d2 a2 alpha2], 'R');
+L(3) = Link([0 d3 a3 alpha3], 'R');
+L(4) = Link([0 d4 a4 alpha4], 'R');
+L(5) = Link([0 d5 a5 alpha5], 'R');
+L(6) = Link([0 d6 a6 alpha6], 'R');
+global Robot
+Robot = SerialLink(L);
 
 %% Arm initialization
 statesArray_AR2_J = [path_AR2_J(1),path_AR2_J(2),path_AR2_J(3)...
@@ -17,7 +32,7 @@ Stepper1.updateStates(statesArray_AR2_J);
 
 command0_AR2_J = path_AR2_J(1:6);
 
-[command0_AR2_W_pos, command0_AR2_W_ori] = AR2fk(command0_AR2_J);
+[command0_AR2_W_pos, command0_AR2_W_ori] = AR2fkine(command0_AR2_J,Robot);
 
 %Gains
 Kp = eye(3);
@@ -56,6 +71,7 @@ r=1/(2*sin(nuef) * [Rif(3,2)-Rif(2,3);Rif(1,3)-Rif(3,1);Rif(2,1)-Rif(1,2);]);   
 %eul vectors are defined by the orentation of the end effector in terms of zyx rotations
     %reference eul angle defined by PHI, THETA, PSI.
 eul0_ref=command0_AR2_W_ori;
+
 euldot0_ref=[0;0;0;];
 
 %these change based on the path
@@ -76,7 +92,7 @@ while(toc<5)
     %on first iteration, initialize variables that constantly update
     if (i == 1)
         %%FIX THESE VARIABLES
-        err = getError_init(command0_AR2_W_pos, eul0_ref, command0_AR2_J);
+        err = getError_init(command0_AR2_W_pos, eul0_ref, command0_AR2_J,Robot);
         ep=err(1:3);
         eo=err(4:6);
         q=command0_AR2_J;
@@ -104,11 +120,11 @@ while(toc<5)
     %timestep
     h = 0.01;
     
-    k_1 = AR2KinDE(x,xdot_ref,theta_ref,thetadot_ref);
+    k_1 = AR2KinDE(x,xdot_ref,theta_ref,thetadot_ref,Robot);
     q_dot=k_1(1:6);
-    k_2 = AR2KinDE(x+0.5*h*k_1,xdot_ref,theta_ref,thetadot_ref);
-    k_3 = AR2KinDE((x+0.5*h*k_2),xdot_ref,theta_ref,thetadot_ref);
-    k_4 = AR2KinDE((x+k_3*h),xdot_ref,theta_ref,thetadot_ref);
+    k_2 = AR2KinDE(x+0.5*h*k_1,xdot_ref,theta_ref,thetadot_ref,Robot);
+    k_3 = AR2KinDE((x+0.5*h*k_2),xdot_ref,theta_ref,thetadot_ref,Robot);
+    k_4 = AR2KinDE((x+k_3*h),xdot_ref,theta_ref,thetadot_ref,Robot);
     x = x + ((1/6)*(k_1+2*k_2+2*k_3+k_4)*h); 
     
 
@@ -134,11 +150,16 @@ while(toc<5)
     Stepper1.updateStates(statesArray_AR2_J);
 
     %update after
-    J = JacobionAR2(q);
+%     J = JacobionAR2(q);
+
+%     J = JacobionAR2tool(q);
     %these will be read from the camera system
 %     xdot_ref=J*qdot;
 %     [~, theta_ref] = AR2fk(q);
 %     [~, thetadot_ref] = AR2fk(qdot);
+
+%     [~, theta_ref] = AR2fkine(q);
+%     [~, thetadot_ref] = AR2fkine(qdot);
     
     i=i+1;
 end
