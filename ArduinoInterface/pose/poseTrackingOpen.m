3438fcc6cabec095766539e4b33ea79d5c4d917a
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2020-10-26 01:35:35 -0400

    Added new analytical jacoians that Ahmed derived at the beginning of this year

--- ArduinoInterface/pose/poseTrackingOpen.m
+++ ArduinoInterface/pose/poseTrackingOpen.m
@@ -13,7 +13,7 @@ d2 = 0;         a2 = 305;   alpha2 = 0;
 d3 = 0;         a3 = 0;     alpha3 = 90*pi/180;     
 d4 = -222.63;   a4 = 0;     alpha4 = -90*pi/180;    
 d5 = 0;         a5 = 0;     alpha5 = 90*pi/180;     
-d6 = -36.25;    a6 = 0;     alpha6 = 0;  
+d6 = -36.25;    a6 = 0;     alpha6 = 0; 
 
 L(1) = Link([0 d1 a1 alpha1], 'R');
 L(2) = Link([0 d2 a2 alpha2], 'R');
