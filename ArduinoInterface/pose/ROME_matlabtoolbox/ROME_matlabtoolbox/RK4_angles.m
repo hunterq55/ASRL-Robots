119c92d98d412970bfbed674f63999636d63d9bf
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2020-11-09 23:44:23 -0500

    RK4 Testing

--- ArduinoInterface/pose/ROME_matlabtoolbox/ROME_matlabtoolbox/RK4_angles.m
+++ ArduinoInterface/pose/ROME_matlabtoolbox/ROME_matlabtoolbox/RK4_angles.m
@@ -17,5 +17,7 @@ for i=1:(length(t)-1)
     k_3 = AR2KinDE((t(i)+0.5*h),(x(i,:)'+0.5*h*k_2));
     k_4 = AR2KinDE((t(i)+h),(x(i,:)'+k_3*h));
     x(i+1,:) = x(i,:) + ((1/6)*(k_1+2*k_2+2*k_3+k_4)*h)'; 
+    
+
 end
 
