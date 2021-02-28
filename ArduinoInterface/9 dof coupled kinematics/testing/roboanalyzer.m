941bf9b670f86d2e30f7de60134ec17f10ffe42f
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-27 23:35:46 -0500

    Started verification of the inverse kinematics

--- ArduinoInterface/9 dof coupled kinematics/testing/roboanalyzer.m
+++ ArduinoInterface/9 dof coupled kinematics/testing/roboanalyzer.m
@@ -30,9 +30,9 @@ dotGV = pTheta * inputVelVector3
                
 pThetaInv = [-2/3 * sin(theta), -2/3 * sin(pi/3-theta), 2/3 * sin(pi/3+theta);
              2/3 * cos(theta), -2/3 * cos(pi/3-theta), 2/3 * cos(pi/3+theta);
-             1/(3*length), 1/(3*length), 1/(3*length);];
+             1/(3*length), 1/(3*length), 1/(3*length);]
 
-Robot.fkine([rev1AngleLock 10 40 theta])
+Robot.fkine([rev1AngleLock 10 40 theta]);
 % Robot.jacob0([revAngleLock 0 0 theta]);
 dotRobot = Robot.jacob0([rev1AngleLock 0 0 theta]) * inputVelVector4;
 
@@ -44,5 +44,5 @@ thetadotRobot = dotRobot(6);
 dotRobotinGV(1) = -sin(theta)*xdotRobot+cos(theta)*ydotRobot+length*thetadotRobot;
 dotRobotinGV(2) = -sin(pi/3-theta)*xdotRobot-cos(pi/3-theta)*ydotRobot+length*thetadotRobot;
 dotRobotinGV(3) = ((xdotRobot*sin(pi/3+theta))-(ydotRobot*cos(pi/3+theta))+length*thetadotRobot);
-dotRobotinGV
+dotRobotinGV;
 
