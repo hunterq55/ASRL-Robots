bb4081c67e1b8605c0f5f6e81f991158a87d8d72
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-28 17:54:57 -0500

    updated JacobianFix script to change the order of inputs, now takes in GV states first to be more in line with the ROMEFK script

--- ArduinoInterface/9 dof coupled kinematics/jacobian derivation/Jacobian Text Fixer/JacobianFix.py
+++ ArduinoInterface/9 dof coupled kinematics/jacobian derivation/Jacobian Text Fixer/JacobianFix.py
@@ -29,11 +29,13 @@ Jac = "function J = J_ROME(theta0)\n"
 Jac = Jac + "d1 = 169.77;    a1 = 64.2;  alpha1 = -90*pi/180;\nd2 = 0;         a2 = 305;   alpha2 = 0;\nd3 = 0;         a3 = 0;     alpha3 = 90*pi/180;\nd4 = -222.63;   a4 = 0;     alpha4 = -90*pi/180;\nd5 = 0;         a5 = 0;     alpha5 = 90*pi/180;\nd6 = -36.25;    a6 = 0;     alpha6 = 0;\n"
 
 for num in range(6):
-    Jac = Jac + "theta"+str(num+1)+" = theta0("+str(num+1)+");\n"
+    Jac = Jac + "theta"+str(num+1)+" = theta0("+str(num+1+3)+");\n"
     Jac = Jac + "cth"+str(num+1)+ " = cos(theta"+str(num+1)+");\n"
     Jac = Jac + "sal"+str(num+1)+ " = sin(alpha"+str(num+1)+");\n"
     Jac = Jac + "sth"+str(num+1)+ " = sin(theta"+str(num+1)+");\n"
     Jac = Jac + "cal"+str(num+1)+ " = cos(alpha"+str(num+1)+");\n\n"
+Jac = Jac + "[EEStates,~] = AR2FKZYZ(theta0(4:end));\n"
+Jac = Jac + "pc6_1 = EEStates(1);\npc6_2 = EEStates(2);\npc6_3 = 0;\npsi = theta0(3);\nL = 0.13*1000;\n"
 Jac = Jac + "psiSin = sin(psi);\n"
 Jac = Jac + "psiCos = cos(psi);\n\n"
 
